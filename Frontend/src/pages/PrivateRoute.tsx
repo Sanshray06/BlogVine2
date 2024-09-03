487c156c2aef4408980d85d84bce1222e8583ee6
Sanshray Mittu <sanshraymittu6@gmail.com>
2024-09-03 09:16:55 +0530

    Your descriptive commit message

--- Frontend/src/pages/PrivateRoute.tsx
+++ Frontend/src/pages/PrivateRoute.tsx
@@ -1,7 +1,8 @@
 
+import { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({ children }) => {
+const PrivateRoute = ({ children } : { children : ReactNode}) => {
   const isAuthenticated = !!localStorage.getItem('token');
 
   return isAuthenticated ? children : <Navigate to="/signup" />;
